various fixes, better debris flux calculation - new debris flux calculation based on local particle velocities and debris volumes - additional state variables for ice and debris volume (supraglacial, englacial, off-glacier) - fixes initial vertical particle position after initial rockfall (now always: r = 1) - small fixes in post-processing scripts (plotting, writing ncdf)

user/code/outputs/plot_debris.py
@@ -90,7 +90,7 @@ def run(cfg, state):
                     c = state.particle["srcid"][::r].numpy() / np.max(state.particle["srcid"].numpy()), # normalized to 1
                     vmin=0,
                     vmax=1,
-                    s=0.5,
+                    s=0.1,
                     cmap="RdBu",
                 )
             else:
@@ -103,7 +103,7 @@ def run(cfg, state):
                     c = state.particle[cfg.outputs.plot_debris.part_var][::r].numpy(),
                     vmin=cfg.outputs.plot_debris.part_var_min,
                     vmax=cfg.outputs.plot_debris.part_var_max,
-                    s=0.5,
+                    s=0.1,
                     cmap="RdBu",
                 )
                     

user/code/outputs/write_ncdf.py
@@ -96,16 +96,17 @@ def run(cfg, state):
                 nc.pyproj_srs = state.pyproj_srs
 
             if hasattr(cfg.processes,'iceflow'):
-                if "Nz" in cfg.processes.debris_cover.tracking:
-                    nc.createDimension("z", cfg.processes.debris_cover.tracking.Nz)
-                    E = nc.createVariable("z", np.dtype("float32").char, ("z",))
-                    E.units = "m"
-                    E.long_name = "z"
-                    E.axis = "Z"
-                    E[:] = np.arange(
-                        cfg.processes.debris_cover.tracking.Nz
-                    )  # TODO: fix this, that's not what we want
-                    print("E:", E)
+                if hasattr(cfg.processes, 'debris_cover'):
+                    if "Nz" in cfg.processes.debris_cover.tracking:
+                        nc.createDimension("z", cfg.processes.debris_cover.tracking.Nz)
+                        E = nc.createVariable("z", np.dtype("float32").char, ("z",))
+                        E.units = "m"
+                        E.long_name = "z"
+                        E.axis = "Z"
+                        E[:] = np.arange(
+                            cfg.processes.debris_cover.tracking.Nz
+                        )  # TODO: fix this, that's not what we want
+                        print("E:", E)
 
             for var in cfg.outputs.write_ncdf.vars_to_save:
                 if hasattr(state, var):

user/code/processes/deb_processes.py
@@ -68,10 +68,30 @@ def initial_rockfall(cfg, state):
         state.nparticle["x"] += diff_x * runout_factor
         state.nparticle["y"] += diff_y * runout_factor
 
-        # Ensure particles remain within the domain
-        state.nparticle["x"] = tf.clip_by_value(state.nparticle["x"], 0, state.x[-1] - state.x[0])
-        state.nparticle["y"] = tf.clip_by_value(state.nparticle["y"], 0, state.y[-1] - state.y[0])
-
+    # Ensure particles remain within the domain
+    state.nparticle["x"] = tf.clip_by_value(state.nparticle["x"], 0, state.x[-1] - state.x[0])
+    state.nparticle["y"] = tf.clip_by_value(state.nparticle["y"], 0, state.y[-1] - state.y[0])
+    
+    # Calculate indices for z interpolation
+    i = state.nparticle["x"] / state.dx
+    j = state.nparticle["y"] / state.dx
+    indices = tf.expand_dims(
+        tf.concat(
+            [tf.expand_dims(j, axis=-1), tf.expand_dims(i, axis=-1)], axis=-1
+        ),
+        axis=0,
+    )
+    state.nparticle["z"] = interpolate_bilinear_tf(
+                tf.expand_dims(tf.expand_dims(state.usurf, axis=0), axis=-1),
+                indices,
+                indexing="ij",
+            )[0, :, 0]
+    state.nparticle["thk"] = interpolate_bilinear_tf(
+                tf.expand_dims(tf.expand_dims(state.thk, axis=0), axis=-1),
+                indices,
+                indexing="ij",
+            )[0, :, 0]
+    
     return state
 
 
@@ -122,7 +142,27 @@ def initial_rockfall_simple(cfg, state):
     # Ensure particles remain within the domain
     state.nparticle["x"] = tf.clip_by_value(state.nparticle["x"], 0, state.x[-1] - state.x[0])
     state.nparticle["y"] = tf.clip_by_value(state.nparticle["y"], 0, state.y[-1] - state.y[0])
-
+    
+    # Calculate indices for z interpolation
+    i = state.nparticle["x"] / state.dx
+    j = state.nparticle["y"] / state.dx
+    indices = tf.expand_dims(
+        tf.concat(
+            [tf.expand_dims(j, axis=-1), tf.expand_dims(i, axis=-1)], axis=-1
+        ),
+        axis=0,
+    )
+    state.nparticle["z"] = interpolate_bilinear_tf(
+                tf.expand_dims(tf.expand_dims(state.usurf, axis=0), axis=-1),
+                indices,
+                indexing="ij",
+            )[0, :, 0]
+    state.nparticle["thk"] = interpolate_bilinear_tf(
+            tf.expand_dims(tf.expand_dims(state.thk, axis=0), axis=-1),
+            indices,
+            indexing="ij",
+        )[0, :, 0]
+    
     return state
 
 def lateral_diffusion(cfg, state):

user/code/processes/deb_seeding.py
@@ -30,6 +30,11 @@ def initialize_seeding(cfg, state):
     state.thk_deb = tf.Variable(tf.zeros_like(state.usurf, dtype=tf.float32))
     state.seeded_particles = tf.Variable([0], dtype=tf.float32)
     state.seeded_debris_volume = tf.Variable([0], dtype=tf.float32)
+    state.vol = tf.Variable([0], dtype=tf.float32)
+    state.surfdebvol = tf.Variable([0], dtype=tf.float32)
+    state.engldebvol = tf.Variable([0], dtype=tf.float32)
+    state.offgldebvol = tf.Variable([0], dtype=tf.float32)
+    
     state.tlast_seeding = -1.0e5000
     state.tcomp_particles = []
     state.particle_counter = tf.Variable([0], dtype=tf.float64)
@@ -181,8 +186,10 @@ def seeding_particles(cfg, state):
         slope_mask = state.slope_rad > (cfg.processes.debris_cover.seeding.slope_threshold / 180 * np.pi)
         # Apply ice thickness threshold
         thk_mask = state.thk < cfg.processes.debris_cover.seeding.thk_threshold
+        # Apply SMB threshold
+        smb_mask = state.smb > cfg.processes.debris_cover.seeding.smb_threshold
         # Combine all masks
-        state.gridseed = tf.logical_and(slope_mask, thk_mask)
+        state.gridseed = tf.logical_and(slope_mask, tf.logical_and(thk_mask, smb_mask))
         if hasattr(state, 'icemask'):
             state.gridseed = tf.logical_and(state.gridseed, state.icemask > 0)
         # For "both" type, combine with shapefile mask
@@ -316,6 +323,12 @@ def seeding_particles(cfg, state):
         state.seeded_particles = tf.size(state.nparticle["x"])
         # Calculate the sum of seeded debris volume
         state.seeded_debris_volume = tf.reduce_sum(state.nparticle["w"])
+        
+        # Calculate total ice volume (vol), surface debris volume (surfdebvol), englacial debris volume (engldebvol) and off-glacier debris volume (offgldebvol) for the current timestep
+        state.vol = tf.reduce_sum(state.thk) * state.dx**2
+        state.surfdebvol = tf.reduce_sum(state.debthick) * state.dx**2
+        state.engldebvol = tf.reduce_sum(state.debcon * state.thk) * state.dx**2
+        state.offgldebvol = tf.reduce_sum(state.debthick_offglacier) * state.dx**2
 
         if cfg.processes.debris_cover.seeding.initial_rockfall == "default":
             from deb_processes import initial_rockfall

user/code/processes/deb_smb_feedback.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 
 from math_utils.getmag import getmag
 from utils import count_particles
+from utils import debris_flux
 
 def deb_thickness(cfg, state):
     if (state.t.numpy() - state.tlast_mb) == 0:
@@ -16,12 +17,7 @@ def deb_thickness(cfg, state):
         state.debcon.assign(tf.reduce_sum(state.engl_w_sum[:-1, :, :], axis=0) / (state.dx**2 * state.thk)) # convert to m depth-averaged volumetric debris concentration by dividing representative volume (m3 debris per particle) by dx^2 (m2 grid cell area) and ice thickness thk
         if "debcon_vert" in cfg.outputs.write_ncdf.vars_to_save:
             state.debcon_vert.assign(tf.where(state.thk[None,:,:] > 0, state.engl_w_sum[:-1, :, :] / (state.dx**2 * state.thk[None,:,:]) * cfg.processes.debris_cover.tracking.Nz, 0.0)) # vertically resolved debris concentration
-        state.debflux_supragl = state.debthick * getmag(state.uvelsurf,state.vvelsurf) # debris flux (supraglacial)
-        if cfg.processes.debris_cover.tracking.Nz != cfg.processes.iceflow.numerics.Nz:
-            engl_w_sum_Nz = tf.reduce_sum(tf.reshape(state.engl_w_sum[:-1, :, :], (-1, int(cfg.processes.debris_cover.tracking.Nz / cfg.processes.iceflow.numerics.Nz), *state.engl_w_sum.shape[1:])), axis=1)
-            state.debflux_engl = tf.reduce_sum(engl_w_sum_Nz * tf.sqrt(state.U**2 + state.V**2), axis=0) / state.dx**2 # debris flux (englacial)
-        else:
-            state.debflux_engl = tf.reduce_sum(state.engl_w_sum[:-1, : ,:] * tf.sqrt(state.U**2 + state.V**2), axis=0) / state.dx**2 # debris flux (englacial)
+        state.debflux_supragl, state.debflux_engl = debris_flux(state) # debris flux (supraglacial and englacial)
         state.debflux.assign(state.debflux_supragl + state.debflux_engl) # debris flux (englacial and supraglacial)
         state.thk_deb.assign(state.thk) # ice thickness at the beginning of the time step
         mask = (state.smb > 0) | (state.thk == 0) # mask out off-glacier areas and accumulation area

user/code/processes/utils.py
@@ -167,7 +167,8 @@ def aggregate_immobile_particles(state):
     count = tf.tensor_scatter_nd_add(zeros, grid_indices, tf.ones_like(immobile_data["t"], dtype=tf.float32))
 
     # Compute means
-    t_mean = tf.math.divide_no_nan(t_sum, count)
+    # t_mean = tf.math.divide_no_nan(t_sum, count)
+    t_mean = tf.zeros_like(state.usurf, dtype=tf.float32)
     englt_mean = tf.math.divide_no_nan(englt_sum, count)
     # srcid_mean = tf.cast(tf.math.divide_no_nan(srcid_sum, tf.cast(count, tf.int32)), tf.int32)
     srcid_mean = tf.zeros_like(state.usurf, dtype=tf.int32)
@@ -271,6 +272,30 @@ def count_particles(cfg, state):
         engl_w_sum = tf.tensor_scatter_nd_add(engl_w_sum, indices_3d, filtered_w)
 
     return engl_w_sum
+
+def debris_flux(state):
+    # compute the flux per particle as the product of particle velocity and particle debris volume
+    state.particle["debflux"] = state.particle["vel"] * state.particle["w"]
+    # aggregate the debris flux of particles in each grid cell
+    grid_particle_x = tf.cast(tf.floor(state.particle["x"] / state.dx), tf.int32)
+    grid_particle_y = tf.cast(tf.floor(state.particle["y"] / state.dx), tf.int32)
+
+    indices = tf.stack([grid_particle_y, grid_particle_x], axis=1)
+    mask_surface = state.particle["r"] >= 0.99
+    mask_engl = state.particle["r"] < 0.99
+
+    debflux_supragl = tf.zeros_like(state.usurf, dtype=tf.float32)
+    debflux_engl = tf.zeros_like(state.usurf, dtype=tf.float32)
+
+    surf_indices = tf.boolean_mask(indices, mask_surface)
+    surf_flux = tf.boolean_mask(state.particle["debflux"], mask_surface)
+    debflux_supragl = tf.tensor_scatter_nd_add(debflux_supragl, surf_indices, surf_flux) / (state.dx**2)
+
+    engl_indices = tf.boolean_mask(indices, mask_engl)
+    engl_flux = tf.boolean_mask(state.particle["debflux"], mask_engl)
+    debflux_engl = tf.tensor_scatter_nd_add(debflux_engl, engl_indices, engl_flux) / (state.dx**2)
+
+    return debflux_supragl, debflux_engl
 
 def print_info_discrete(state):
     if state.t % 50 == 0: